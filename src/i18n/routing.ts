8a5e19758de3324b6f17154b523fb3899e136971
karolinakuzniewicz <kkuzniewicz@gmail.com>
2024-09-06 21:43:52 +0200

    Init

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,20 +1,29 @@
-import {createLocalizedPathnamesNavigation} from 'next-intl/navigation';
-import {defineRouting} from 'next-intl/routing';
+import { createLocalizedPathnamesNavigation } from "next-intl/navigation";
+import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ['en', 'de'],
-  defaultLocale: 'en',
+  locales: ["en-GB", "en-US", "es-US", "de-AT"],
+  defaultLocale: "en-GB",
+  localePrefix: {
+    mode: "always",
+    prefixes: {
+      "es-US": "/us/es",
+      "de-AT": "/de/at",
+      "en-GB": "/gb",
+      "en-US": "/us",
+    },
+  },
   pathnames: {
-    '/': '/',
-    '/pathnames': {
-      en: '/pathnames',
-      de: '/pfadnamen'
-    }
-  }
+    // "/": "/",
+    // "/pathnames": {
+    //   "en-GB": "/gb/pathnames",
+    //   "de-AT": "/de/at/pfadnamen",
+    // },
+  },
 });
 
 export type Pathnames = keyof typeof routing.pathnames;
 export type Locale = (typeof routing.locales)[number];
 
-export const {Link, getPathname, redirect, usePathname, useRouter} =
+export const { Link, getPathname, redirect, usePathname, useRouter } =
   createLocalizedPathnamesNavigation(routing);
